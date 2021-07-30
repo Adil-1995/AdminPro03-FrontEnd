b9317da0e5f184aff6e0336e56cd30829f5a6cdc
Adil-1995 <achairadil@gmail.com>
2021-07-30 20:28:03 +0200

    change repo

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,32 @@
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base_url;
+
+export class Usuario {
+
+    constructor(
+        public nombre: string,
+        public email: string,
+        public password?: string,
+        public img?: string,
+        public google?: boolean,
+        public role?: string,
+        public uid?: string,
+    ) {}
+
+    get imagenUrl() {
+      console.log(this.img);
+
+
+        if ( this.img.includes('https') ) {
+            return this.img;
+        }
+
+        if ( this.img ) {
+            return `${ base_url }/upload/usuarios/${ this.img }`;
+        } else {
+            return `${ base_url }/upload/usuarios/no-image`;
+        }
+    }
+}
+
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+  public usuario: Usuario;
+
+  constructor( private usuarioService: UsuarioService ) {
+    this.usuario = usuarioService.usuario;
+  }
+
+  logout() {
+    this.usuarioService.logout();
+  }
+
+
+}
