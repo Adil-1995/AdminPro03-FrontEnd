7f9c43538a3b49a1030535a8d0d4db5da80ba4e5
Adil-1995 <achairadil@gmail.com>
2021-08-08 09:52:16 +0200

    edit deploy

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,28 +1,47 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  CanLoad,
+  Route,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
 import { UsuarioService } from '../services/usuario.service';
 import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
-
-  constructor( private usuarioService: UsuarioService,
-               private router: Router) {}
-
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
-
-      return this.usuarioService.validarToken()
-        .pipe(
-          tap( estaAutenticado =>  {
-            if ( !estaAutenticado ) {
-              this.router.navigateByUrl('/login');
-            }
-          })
-        );
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
   }
-  
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 }
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,24 +1,10 @@
-import { AdminGuard } from './../guards/admin.guard';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AuthGuard } from '../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
 
-//Matenimientos
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
 
 
 
@@ -27,24 +13,8 @@ const routes: Routes = [
         path: 'dashboard',
         component: PagesComponent,
         canActivate: [ AuthGuard ],
-        children: [
-            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
-            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' }},
-            { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'busquedas' }},
-            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' }},
-            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica #1' }},
-            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
-            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }},
-            { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' }},
-            //Matenimientos
-            { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Matenimiento de Hospitales' }},
-            { path: 'medicos', component: MedicosComponent, data: { titulo: 'Matenimiento de Médicos ' }},
-            { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Matenimiento de Médicos ' }},
-
-            // Rutas de ADMIN
-            { path: 'usuarios', canActivate: [AdminGuard], component: UsuariosComponent, data: { titulo: 'Matenimiento de Usuarios' }},
-
-        ]
+        canLoad: [AuthGuard],
+        loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule)
     },
 ];
 
