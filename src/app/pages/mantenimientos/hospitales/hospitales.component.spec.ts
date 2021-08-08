383491f18f9154936d779b1afb36cb8df12a3cc7
Adil-1995 <achairadil@gmail.com>
2021-08-08 12:00:58 +0200

    deploy after update cli

--- src/app/pages/mantenimientos/hospitales/hospitales.component.spec.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HospitalesComponent } from './hospitales.component';
 
@@ -6,7 +6,7 @@ describe('HospitalesComponent', () => {
   let component: HospitalesComponent;
   let fixture: ComponentFixture<HospitalesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HospitalesComponent ]
     })
