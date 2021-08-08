e881293a4ec0d8f617f2c0a7e5bef5782284734f
Adil-1995 <achairadil@gmail.com>
2021-08-08 11:26:48 +0200

    deploy 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,12 @@
 
 export const environment = {
   production: false,
-  base_url: 'https://adminpro-backend2021.herokuapp.com/api'
+  base_url: 'https://mi-admin-pro.herokuapp.com/api'
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
